bcd616f7ee3f8dc19c1637a0cb1e36534fa874a0
MarcusDavidG <davidmarcus827@gmail.com>
2026-01-13 14:24:35 +0100

    feat: implement useInterval hook

--- frontend/src/hooks/useInterval.ts
+++ frontend/src/hooks/useInterval.ts
@@ -1,6 +1,16 @@
-import { useState } from 'react';
+import { useEffect, useRef } from 'react'
 
-export function useInterval() {
-  const [state, setState] = useState();
-  return { state, setState };
+export function useInterval(callback: () => void, delay: number | null) {
+  const savedCallback = useRef(callback)
+
+  useEffect(() => {
+    savedCallback.current = callback
+  }, [callback])
+
+  useEffect(() => {
+    if (delay === null) return
+
+    const id = setInterval(() => savedCallback.current(), delay)
+    return () => clearInterval(id)
+  }, [delay])
 }
