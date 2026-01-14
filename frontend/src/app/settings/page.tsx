399940220d7865108bd677465bf5c7559d011895
MarcusDavidG <davidmarcus827@gmail.com>
2026-01-14 23:11:06 +0100

    feat: update settings page content

--- frontend/src/app/settings/page.tsx
+++ frontend/src/app/settings/page.tsx
@@ -1,3 +1,7 @@
+/**
+ * This is the settings page.
+ * @returns A page to manage user settings.
+ */
 export default function SettingsPage() {
-  return <div>settings page</div>
+  return <div>This is the page to manage user settings.</div>
 }
